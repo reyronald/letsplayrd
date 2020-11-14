86f0c71a77781e91b8759ce47841af2c06dda3d2
Ronald Rey <reyronald@gmail.com>
2020-11-14 15:07:18 -0400

    add png

--- pages/_app.js
+++ pages/_app.js
@@ -5,14 +5,19 @@ import '../styles/globals.css'
 function MyApp({ Component, pageProps }) {
   React.useEffect(() => {
     if ('serviceWorker' in window.navigator) {
-      navigator.serviceWorker
-        .register('/service-worker.js')
-        .then(function (registration) {
-          console.log('Registration successful, scope is:', registration.scope)
-        })
-        .catch(function (error) {
-          console.log('Service worker registration failed, error:', error)
-        })
+      window.addEventListener('load', () => {
+        window.navigator.serviceWorker
+          .register('/service-worker.js')
+          .then(function (registration) {
+            console.log(
+              'Registration successful, scope is:',
+              registration.scope,
+            )
+          })
+          .catch(function (error) {
+            console.log('Service worker registration failed, error:', error)
+          })
+      })
     }
   }, [])
 
